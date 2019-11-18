322cebb1cff5c57756606a87483565c8b1b662af
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-11-18 13:49:49 -0800

    Adapt showdoc

--- local/imports.py
+++ local/imports.py
@@ -2,5 +2,6 @@ import os,re,json,glob,collections,pickle,shutil
 from pathlib import Path
 from textwrap import TextWrapper
 import numpy as np
+from typing import Union,Optional
 
 def test_eq(a,b): assert a==b, f'{a}, {b}'
