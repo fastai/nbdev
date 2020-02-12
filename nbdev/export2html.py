45cf6619dc5d0851334d61f910bea625a8a87cd4
Hamel Husain <hamel.husain@gmail.com>
2020-02-12 14:09:57 -0800

    corrected conflicts

--- nbdev/export2html.py
+++ nbdev/export2html.py
@@ -99,11 +99,9 @@ def _to_html(text):
 def add_jekyll_notes(cell):
     "Convert block quotes to jekyll notes in `cell`"
     styles = Config().get('jekyll_styles', 'note,warning,tip,important').split(',')
-    _styles = ['note', 'warning', 'tip', 'important']
     def _inner(m):
         title,text = m.groups()
         if title.lower() not in styles: return f"> {m.groups()[0]}: {m.groups()[1]}"
-        if title.lower() not in _styles: return f"> {m.groups()[0]}: {m.groups()[1]}"
         return '{% include '+title.lower()+".html content=\'"+_to_html(text)+"\' %}"
     if cell['cell_type'] == 'markdown':
         cell['source'] = _re_block_notes.sub(_inner, cell['source'])
