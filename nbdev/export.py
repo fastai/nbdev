ec233339689fc4e848102a8f79d3b8d66d01f00d
Peter Butterfill <peter.butterfill@gmail.com>
2020-07-30 13:18:08 +0100

    found a way to import from __future__ with magic flags

--- nbdev/export.py
+++ nbdev/export.py
@@ -240,16 +240,17 @@ def extra_add(flags, code):
 # Cell
 _re_from_future_import = re.compile(r"^from[ \t]+__future__[ \t]+import.*$", re.MULTILINE)
 
-def _from_future_import(fname, code, to_dict=None):
+def _from_future_import(fname, flags, code, to_dict=None):
     "Write `__future__` imports to `fname` and return `code` with `__future__` imports commented out"
-    m=check_re({'source': code}, _re_from_future_import, False)
-    if not m: return code
-    if to_dict is not None: return m.re.sub('#nbdev_comment \g<0>', code)
+    from_future_imports = _re_from_future_import.findall(code)
+    if from_future_imports: code = _re_from_future_import.sub('#nbdev_comment \g<0>', code)
+    else: from_future_imports = _re_from_future_import.findall(flags)
+    if not from_future_imports or to_dict is not None: return code
     with open(fname, 'r', encoding='utf8') as f: text = f.read()
     start = _re__all__def.search(text).start()
     with open(fname, 'w', encoding='utf8') as f:
-        f.write('\n'.join([text[:start], *_re_from_future_import.findall(code), '\n', text[start:]]))
-    return m.re.sub('#nbdev_comment \g<0>', code)
+        f.write('\n'.join([text[:start], *from_future_imports, '\n', text[start:]]))
+    return code
 
 # Cell
 def _add2all(fname, names, line_width=120):
@@ -345,7 +346,7 @@ def split_flags_and_code(cell, return_type=list):
     split_pos = 0 if code_lines[0].strip().startswith('#') else -1
     for i, line in enumerate(code_lines):
         if line.startswith('%nbdev_'): split_pos=i
-        elif not line.startswith('#') and line.strip(): break
+        elif not line.startswith('#') and line.strip() and not _re_from_future_import.match(line): break
     split_pos+=1
     res = code_lines[:split_pos], code_lines[split_pos:]
     if return_type is list: return res
@@ -396,8 +397,9 @@ def _notebook2script(fname, modules, silent=False, to_dict=None):
         code_lines = _deal_import(code_lines, fname_out)
         code = sep + orig + '\n'.join(code_lines)
         names = export_names(code)
-        extra,code = extra_add('\n'.join(flag_lines), code)
-        code = _from_future_import(fname_out, code, to_dict)
+        flags = '\n'.join(flag_lines)
+        extra,code = extra_add(flags, code)
+        code = _from_future_import(fname_out, flags, code, to_dict)
         if a:
             if to_dict is None: _add2all(fname_out, [f"'{f}'" for f in names if '.' not in f and len(f) > 0] + extra)
         mod.index.update({f: fname.name for f in names})
