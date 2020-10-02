e91d4ef765c3cc2f418e857579ac0414ccbe866c
Butch Landingin <butchland@gmail.com>
2020-10-03 02:08:46 +0800

    Fix filter condition to match if filename is not 'index.ipynb'

--- nbdev/export2html.py
+++ nbdev/export2html.py
@@ -706,7 +706,7 @@ def _create_default_sidebar():
     dic = {"Overview": "/"}
     files = [f for f in Config().nbs_path.glob('**/*.ipynb') if not f.name.startswith('_')]
     fnames = [_nb2htmlfname(f) for f in sorted(files)]
-    titles = [_get_title(f) for f in fnames if 'index' not in f.stem!='index']
+    titles = [_get_title(f) for f in fnames if f.stem!='index']
     if len(titles) > len(set(titles)): print(f"Warning: Some of your Notebooks use the same title ({titles}).")
     dic.update({_get_title(f):f'{f.name}' for f in fnames if f.stem!='index'})
     return dic
