447de79c9c910acb720cf46ceb64c0c5bd22669a
feras-oughali <oughali.feras@gmail.com>
2021-02-16 10:31:25 +0300

    change flags to nbdev comments

--- nbdev/_nbdev.py
+++ nbdev/_nbdev.py
@@ -111,9 +111,9 @@ index = {"read_nb": "00_export.ipynb",
          "clean_cr": "07_clean.ipynb",
          "BSLASH": "07_clean.ipynb",
          "nbdev_clean_nbs": "07_clean.ipynb",
-         "S1": "flags_page.ipynb",
-         "S2": "flags_page.ipynb",
-         "S3": "flags_page.ipynb",
+         "S1": "nbdev_comments.ipynb",
+         "S2": "nbdev_comments.ipynb",
+         "S3": "nbdev_comments.ipynb",
          "say_hello": "tutorial.ipynb",
          "HelloSayer": "tutorial.ipynb"}
 
@@ -125,7 +125,7 @@ modules = ["export.py",
            "merge.py",
            "cli.py",
            "clean.py",
-           "flags_page.py",
+           "nbdev_comments.py",
            "tutorial.py"]
 
 doc_url = "https://nbdev.fast.ai/"
