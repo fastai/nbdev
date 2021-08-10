380729c2bca46f9879c5fa1109dbbe59d72107ff
Jeremy Howard <j@fast.ai>
2021-08-10 14:42:15 +1000

    baseurl

--- nbprocess/_nbdev.py
+++ nbprocess/_nbdev.py
@@ -2,42 +2,44 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"NbCell": "00_read.ipynb",
+index = {"display_json": "00_read.ipynb",
+         "NbCell": "00_read.ipynb",
          "dict2nb": "00_read.ipynb",
          "read_nb": "00_read.ipynb",
          "nbprocess_create_config": "00_read.ipynb",
          "get_config": "00_read.ipynb",
          "add_init": "00_read.ipynb",
-         "export_cells": "00_read.ipynb",
+         "write_cells": "00_read.ipynb",
          "basic_export_nb": "00_read.ipynb",
-         "extract_comments": "01_export.ipynb",
-         "NotebookProcessor": "01_export.ipynb",
-         "NotebookProcessor.process_comment": "01_export.ipynb",
-         "NotebookProcessor.process_cell": "01_export.ipynb",
-         "NotebookProcessor.no_cmd": "01_export.ipynb",
-         "NotebookProcessor.process": "01_export.ipynb",
-         "find_var": "01_export.ipynb",
-         "read_var": "01_export.ipynb",
-         "update_var": "01_export.ipynb",
-         "ModuleMaker": "01_export.ipynb",
-         "retr_exports": "01_export.ipynb",
-         "ModuleMaker.make_all": "01_export.ipynb",
-         "relative_import": "01_export.ipynb",
-         "absolute_import": "01_export.ipynb",
-         "update_import": "01_export.ipynb",
-         "NbCell.import2relative": "01_export.ipynb",
-         "ModuleMaker.make": "01_export.ipynb",
-         "ExportModuleProcessor": "01_export.ipynb",
-         "ExportModuleProcessor.default_exp_code": "01_export.ipynb",
-         "ExportModuleProcessor.exporti_code": "01_export.ipynb",
-         "ExportModuleProcessor.export_code": "01_export.ipynb",
-         "ExportModuleProcessor.exports_code": "01_export.ipynb",
-         "ExportModuleProcessor.create_modules": "01_export.ipynb"}
+         "find_var": "01_maker.ipynb",
+         "read_var": "01_maker.ipynb",
+         "update_var": "01_maker.ipynb",
+         "ModuleMaker": "01_maker.ipynb",
+         "retr_exports": "01_maker.ipynb",
+         "ModuleMaker.make_all": "01_maker.ipynb",
+         "relative_import": "01_maker.ipynb",
+         "absolute_import": "01_maker.ipynb",
+         "update_import": "01_maker.ipynb",
+         "NbCell.import2relative": "01_maker.ipynb",
+         "ModuleMaker.make": "01_maker.ipynb",
+         "basic_export_nb2": "01_maker.ipynb",
+         "extract_comments": "02_export.ipynb",
+         "NotebookProcessor": "02_export.ipynb",
+         "NotebookProcessor.process_comment": "02_export.ipynb",
+         "NotebookProcessor.process_cell": "02_export.ipynb",
+         "NotebookProcessor.no_cmd": "02_export.ipynb",
+         "NotebookProcessor.process": "02_export.ipynb",
+         "ExportModuleProcessor": "02_export.ipynb",
+         "ExportModuleProcessor.default_exp_code": "02_export.ipynb",
+         "ExportModuleProcessor.exporti_code": "02_export.ipynb",
+         "ExportModuleProcessor.export_code": "02_export.ipynb",
+         "ExportModuleProcessor.create_modules": "02_export.ipynb"}
 
 modules = ["read.py",
+           "maker.py",
            "export.py"]
 
-doc_url = "https://fastai.github.io/nbprocess/"
+doc_url = "https://nbprocess.github.io/"
 
 git_url = "https://github.com/fastai/nbprocess/tree/master/"
 
