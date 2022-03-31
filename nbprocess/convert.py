6b839b234391f607f568ba0bc7cf93375ddddcd0
Jeremy Howard <j@fast.ai>
2022-03-31 13:55:02 +1000

    updates

--- nbprocess/convert.py
+++ nbprocess/convert.py
@@ -9,27 +9,13 @@ from nbconvert.exporters import Exporter
 from fastcore.all import Path,parallel,call_parse,bool_arg,globtastic
 
 # %% auto 0
-__all__ = ['nb2md', 'parallel_nb2md', 'nbdoc_build']
+__all__ = ['nbprocess_docs']
 
-# %% ../nbs/07_convert.ipynb 4
-def nb2md(fname:str|Path, exp:Exporter):
-    "Convert a notebook in `fname` to a markdown file."
-    file = Path(fname)
-    assert file.name.endswith('.ipynb'), f'{str(fname)} is not a notebook.'
-    assert file.is_file(), f'file {str(fname)} not found.'
-    print(f"converting: {str(file)}")
-    try:
-        o,r = exp.from_filename(fname)
-        file.with_suffix('.md').write_text(o)
-        return True
-    except Exception as e:
-        print(e)
-        return False
-
-# %% ../nbs/07_convert.ipynb 7
+# %% ../nbs/07_convert.ipynb 5
 @call_parse
-def parallel_nb2md(
+def nbprocess_docs(
     path:str='.', # path or filename
+    dest:str='build', # path or filename
     recursive:bool=True, # search subfolders
     symlinks:bool=True, # follow symlinks?
     n_workers:int=None, # Number of parallel workers
@@ -43,7 +29,6 @@ def parallel_nb2md(
     skip_folder_re:str='^[_.]' # Skip folders matching regex
 ):
     if os.environ.get('IN_TEST',0): return
-    exp=get_mdx_exporter()
     if not recursive: skip_folder_re='.'
     files = globtastic(path, symlinks=symlinks, file_glob=file_glob, file_re=file_re,
                        folder_re=folder_re, skip_file_glob=skip_file_glob,
@@ -56,28 +41,11 @@ def parallel_nb2md(
         files,_files = [],files.copy()
         for fname in _files:
             fname_out = fname.with_suffix('.md')
-            if not fname_out.exists() or os.path.getmtime(fname) >= os.path.getmtime(fname_out):
-                files.append(fname)
+            if not fname_out.exists() or os.path.getmtime(fname) >= os.path.getmtime(fname_out): files.append(fname)
     if len(files)==0: print("No notebooks were modified.")
     else:
         if sys.platform == "win32": n_workers = 0
-        passed = parallel(nb2md, files, n_workers=n_workers, exp=exp, pause=pause)
+        passed = parallel(nb2md, files, n_workers=n_workers, pause=pause, dest=dest)
         if not all(passed):
             msg = "Conversion failed on the following:\n"
             print(msg + '\n'.join([f.name for p,f in zip(passed,files) if not p]))
-
-# %% ../nbs/07_convert.ipynb 16
-@call_parse
-def nbdoc_build(
-    srcdir:str=None,  # A directory of notebooks to convert to docs recursively, can also be a filename.
-    force_all:bool_arg=False, # Rebuild even notebooks that havent changed
-    n_workers:int=None,  # Number of workers to use
-    pause:float=0.5  # Pause time (in secs) between notebooks to avoid race conditions
-):
-    "Build the documentation by converting notebooks in `srcdir` to markdown"
-    parallel_nb2md(basedir=srcdir, 
-                   exp=get_mdx_exporter(), 
-                   recursive=True, 
-                   force_all=force_all, 
-                   n_workers=n_workers, 
-                   pause=pause)
