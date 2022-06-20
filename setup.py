d57e7385a5e8472be6a7671f27285a9bffcb434a
muellerzr <muellerzr@gmail.com>
2022-06-20 18:16:55 -0400

    37 requirements

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
     packages = setuptools.find_packages(),
     include_package_data = True,
     install_requires = requirements,
-    extras_require={ 'dev': dev_requirements },
+    extras_require={ 'dev': dev_requirements, ':python_version=="3.7"':['asttokens']},
     dependency_links = cfg.get('dep_links','').split(),
     python_requires  = '>=' + cfg['min_python'],
     long_description = open('README.md').read(),
