e0e54217ac18dc52e1d2f1669ee85bba094477f3
seem <mwlorgat@gmail.com>
2022-08-07 11:34:58 +1000

    fix #755

--- nbdev/read.py
+++ nbdev/read.py
@@ -12,6 +12,7 @@ from fastcore.basics import *
 from fastcore.imports import *
 from fastcore.meta import *
 from fastcore.script import *
+from fastcore.xdg import *
 from fastcore.xtras import *
 
 import ast,functools
@@ -30,18 +31,10 @@ def create_output(txt, mime):
 def show_src(src, lang='python'): return Markdown(f'```{lang}\n{src}\n```')
 
 # %% ../nbs/01_read.ipynb 10
-@patch
-def __init__(self:Config, cfg_path, cfg_name, create=None, save=True):
-    cfg_path = Path(cfg_path).expanduser().absolute()
-    self.config_path,self.config_file = cfg_path,cfg_path/cfg_name
-    if self.config_file.exists(): self.d = read_config_file(self.config_file)
-    elif create is not None:
-        self.d = ConfigParser(create)['DEFAULT']
-        if save:
-            cfg_path.mkdir(exist_ok=True, parents=True)
-            self.save()
+_nbdev_home_dir = 'nbdev' # sub-directory of xdg base dir
+_nbdev_config_name = 'settings.ini'
 
-# %% ../nbs/01_read.ipynb 13
+# %% ../nbs/01_read.ipynb 11
 def apply_defaults(
     cfg,
     lib_name:str=None, # Package name, defaults to local repo folder name
@@ -78,7 +71,7 @@ def apply_defaults(
         if not (k.startswith('_') or k=='cfg' or k in cfg): cfg[k] = v
     return cfg
 
-# %% ../nbs/01_read.ipynb 14
+# %% ../nbs/01_read.ipynb 12
 @call_parse
 @delegates(apply_defaults, but='cfg')
 def nbdev_create_config(
@@ -87,7 +80,7 @@ def nbdev_create_config(
     author_email:str, # Package author's email address
     description:str, # Short summary of the package
     path:str='.', # Path to create config file
-    cfg_name:str='settings.ini', # Name of config file to create
+    cfg_name:str=_nbdev_config_name, # Name of config file to create
     **kwargs
 ):
     "Create a config file"
@@ -96,27 +89,38 @@ def nbdev_create_config(
     cfg = apply_defaults(cfg, **kwargs)
     cfg.save()
 
-# %% ../nbs/01_read.ipynb 16
-@functools.lru_cache(maxsize=None)
-def get_config(cfg_name='settings.ini', path=None):
-    "`Config` for ini file found in `path` (defaults to `cwd`)"
+# %% ../nbs/01_read.ipynb 14
+def _nbdev_config_file(cfg_name=_nbdev_config_name, path=None):
     cfg_path = path = Path.cwd() if path is None else Path(path)
     while cfg_path != cfg_path.parent and not (cfg_path/cfg_name).exists(): cfg_path = cfg_path.parent
     if not (cfg_path/cfg_name).exists(): cfg_path = path
-    cfg = Config(cfg_path, cfg_name=cfg_name, create={}, save=False)
+    return cfg_path/cfg_name
+
+# %% ../nbs/01_read.ipynb 16
+def _xdg_config_paths(cfg_name=_nbdev_config_name):
+    xdg_config_paths = reversed([xdg_config_home()]+xdg_config_dirs())
+    return [o/_nbdev_home_dir/cfg_name for o in xdg_config_paths]
+
+# %% ../nbs/01_read.ipynb 17
+@functools.lru_cache(maxsize=None)
+def get_config(cfg_name=_nbdev_config_name, path=None):
+    "`Config` for ini file found in `path` (defaults to `cwd`)"
+    cfg_file = _nbdev_config_file(cfg_name, path)
+    extra_files = _xdg_config_paths(cfg_name)
+    cfg = Config(cfg_file.parent, cfg_file.name, extra_files=extra_files)
     return apply_defaults(cfg)
 
-# %% ../nbs/01_read.ipynb 20
+# %% ../nbs/01_read.ipynb 21
 def config_key(c, default=None, path=True, missing_ok=None):
     "Look for key `c` in settings.ini and fail gracefully if not found and no default provided"
     if missing_ok is not None:
         warn("`missing_ok` is no longer used. Don't pass it to `config_key` to silence this warning.")
     cfg = get_config()
     res = cfg.path(c, default) if path else cfg.get(c, default)
-    if res is None: raise ValueError(f'`{c}` not specified in settings.ini')
+    if res is None: raise ValueError(f'`{c}` not specified in {_nbdev_config_name}')
     return res
 
-# %% ../nbs/01_read.ipynb 23
+# %% ../nbs/01_read.ipynb 24
 _init = '__init__.py'
 
 def _has_py(fs): return any(1 for f in fs if f.endswith('.py'))
@@ -132,13 +136,13 @@ def add_init(path):
         subds = (os.listdir(r/d) for d in ds)
         if _has_py(fs) or any(filter(_has_py, subds)) and not (r/_init).exists(): (r/_init).touch()
 
-# %% ../nbs/01_read.ipynb 27
+# %% ../nbs/01_read.ipynb 28
 def write_cells(cells, hdr, file, offset=0):
     "Write `cells` to `file` along with header `hdr` starting at index `offset` (mainly for nbdev internal use)"
     for cell in cells:
         if cell.source.strip(): file.write(f'\n\n{hdr} {cell.idx_+offset}\n{cell.source}')
 
-# %% ../nbs/01_read.ipynb 28
+# %% ../nbs/01_read.ipynb 29
 def basic_export_nb(fname, name, dest=None):
     "Basic exporter to bootstrap nbdev"
     if dest is None: dest = config_key('lib_path')
