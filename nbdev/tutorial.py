7b78d619b1a9dc2a83c83135fe9d21275f60340f
seem <mwlorgat@gmail.com>
2022-08-12 10:57:28 +1000

    refactor tutorial: make your first edit

--- nbdev/tutorial.py
+++ nbdev/tutorial.py
@@ -3,12 +3,12 @@
 # %% auto 0
 __all__ = ['say_hello', 'HelloSayer']
 
-# %% ../nbs/tutorial.ipynb 62
+# %% ../nbs/tutorial.ipynb 74
 def say_hello(to):
     "Say hello to somebody"
     return f'Hello {to}!'
 
-# %% ../nbs/tutorial.ipynb 86
+# %% ../nbs/tutorial.ipynb 97
 class HelloSayer:
     "Say hello to `to` using `say_hello`"
     def __init__(self, to): self.to = to
