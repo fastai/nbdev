16fc8dd0929444745a274ff1af2288ac82fecca2
Jeremy Howard <j@fast.ai>
2022-08-21 22:09:57 +1000

    rm line nos

--- setup.py
+++ setup.py
@@ -66,7 +66,6 @@ setuptools.setup(
     zip_safe = False,
     entry_points = {
         'console_scripts': cfg.get('console_scripts','').split(),
-        'mkdocs.plugins': [ 'rm_num_prefix = nbdev.mkdocs:RmNumPrefix' ],
         'nbdev': [f'{cfg.get("lib_path")}={cfg.get("lib_path")}._modidx:d']
     },
     project_urls = project_urls,
