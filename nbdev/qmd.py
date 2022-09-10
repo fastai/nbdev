12e2ffdbcb40602e9d5d45f2c99bcd980bcdd235
Jeremy Howard <j@fast.ai>
2022-09-10 13:37:27 +1000

    simplify renderscript.qmd.py

--- nbdev/qmd.py
+++ nbdev/qmd.py
@@ -8,7 +8,7 @@ from fastcore.utils import *
 from fastcore.meta import delegates
 
 # %% auto 0
-__all__ = ['meta', 'div', 'img', 'btn', 'tblrow', 'tblhdr']
+__all__ = ['meta', 'div', 'img', 'btn', 'tbl_row', 'tbl_sep']
 
 # %% ../nbs/api/qmd.ipynb 4
 def meta(md,  # Markdown to add meta to
@@ -61,12 +61,14 @@ def btn(txt, # Button text
     return meta(f'[{txt}]({link})', classes=classes, style=style, role="button")
 
 # %% ../nbs/api/qmd.ipynb 8
-def tblrow(cols):
-    "Create a table row from `cols`"
+def tbl_row(cols:list,  # Auto-stringified columns to show in the row
+           ):
+    "Create a markdown table row from `cols`"
     return '|' + '|'.join(str(c or '') for c in cols) + '|'
 
 # %% ../nbs/api/qmd.ipynb 9
-def tblhdr(titles, sizes=None):
-    "Create a table header with `titles` and relative size `sizes`"
-    if sizes is None: sizes = [3]*len(titles)
-    return [tblrow(titles), tblrow('-'*s for s in sizes)]
+def tbl_sep(sizes:int|list=3  # List of column sizes, or single `int` if all sizes the same
+           ):
+    "Create a markdown table separator with relative column size `sizes`"
+    if isinstance(sizes,int): sizes = [3]*sizes
+    return tbl_row('-'*s for s in sizes)
--- nbs/tutorials/renderscript.qmd.py
+++ nbs/tutorials/renderscript.qmd.py
@@ -2,11 +2,10 @@
 title: RenderScripts
 ---"""
 
-from nbdev.qmd import *
-def im(fn, width, **kw): return img(f"/images/{fn}", width=f"{width}%", **kw)
-r=[]; a=r.append
+from nbdev import qmd
+def im(fn, width, **kw): return qmd.img(f"/images/{fn}", width=f"{width}%", **kw)
 
-a("""*RenderScripts* are regular Python scripts, except that:
+print("""*RenderScripts* are regular Python scripts, except that:
 
 - Rather than just having the extension `.py`, they have an extension like `.qmd.py`
 - They contain a module docstring containing [frontmatter](https://quarto.org/docs/tools/jupyter-lab.html#yaml-front-matter) (i.e three hyphens on a line, then some yaml, then another three hyphens on a line).
@@ -16,9 +15,10 @@ These scripts are run when your site is rendered. Anything that they print to st
 Hot/live reloading even works with these .py scripts -- so as soon as you save the script, you'll see the new output in your web browser.
 
 This approach can be particularly helpful for generating data-driven documents. For instance, consider this table, containing a list of the people with testimonials on nbdev's home page:
+
 """)
 
-testms = [
+testimonials = [
     ('chris-lattner.png', 'Chris Lattner', 'Inventor of Swift and LLVM'),
     ('fernando-pérez.jpeg', 'Fernando Pérez', 'Creator of Jupyter'),
     ('david-berg.jpeg', 'David Berg', 'Software Engineer, Netflix'),
@@ -26,15 +26,18 @@ testms = [
     ('roxanna-pourzand.jpeg', 'Roxanna Pourzand', 'Product Manager, Transform'),
     ('hugo-bowne-anderson.jpeg', 'Hugo Bowne-Anderson', 'Head of Developer Relations, Outerbounds')
 ]
-r += tblhdr(['','Name','Position'], [1,3,4])
-r += [tblrow([im(fn, 60), nm, ps]) for fn,nm,ps in testms]
+print(qmd.tbl_row(['','Name','Position']))
+print(qmd.tbl_sep([1,3,4]))
+for fname,name,position in testimonials:
+    print(qmd.tbl_row([im(fname, 60), name, position]))
 
-a("""
+print("""
+
 When creating a table like this, it can be tricky to ensure that markdown is correct and consistent for every row. It can be easier and more maintainable to programatically generate it. The table above is generated from the following python list:
 
 ::: {.column-screen-inset-right}
 ```python
-testms = [
+testimonials = [
     ('chris-lattner.png', 'Chris Lattner', 'Inventor of Swift and LLVM'),
     ('fernando-pérez.jpeg', 'Fernando Pérez', 'Creator of Jupyter'),
     ('david-berg.jpeg', 'David Berg', 'Software Engineer, Netflix'),
@@ -45,22 +48,23 @@ testms = [
 ```
 :::
 
-To produce the table from this python list, the following two lines of code are used:
+To produce the table from this python list, the following four lines of code are used:
 
 ```python
-r += tblhdr(['','Name','Position'], [1,3,4])
-r += [tblrow([im(fn, 60), nm, ps]) for fn,nm,ps in testms]
+print(qmd.tbl_row(['','Name','Position']))
+print(qmd.tbl_sep([1,3,4]))
+for fname,name,position in testimonials:
+    print(qmd.tbl_row([im(fname, 60), name, position]))
 ```
 
-`r` is a list which is printed at the end of the script, and [`tblhdr`](https://nbdev.fast.ai/api/qmd.html#tblhdr) and [`tblrow`](https://nbdev.fast.ai/api/qmd.html#tblrow) are two functions imported from the module `nbdev.qmd`. [`nbdev.qmd`](https://nbdev.fast.ai/api/qmd.html) is a small module that has some convenient functions for creating `.qmd` documents, such as the table creation functions used above. You can see more examples of their use in [index.qmd.py](https://github.com/fastai/nbdev/blob/master/nbs/index.qmd.py), which is the RenderScript which creates the [nbdev home page](https://nbdev.fast.ai).
+[`tbl_hdr`](https://nbdev.fast.ai/api/qmd.html#tbl_hdr) and [`tbl_row`](https://nbdev.fast.ai/api/qmd.html#tbl_row) are two functions imported from the module `nbdev.qmd`. [`nbdev.qmd`](https://nbdev.fast.ai/api/qmd.html) is a small module that has some convenient functions for creating `.qmd` documents, such as the table creation functions used above. You can see more examples of their use in [index.qmd.py](https://github.com/fastai/nbdev/blob/master/nbs/index.qmd.py), which is the RenderScript which creates the [nbdev home page](https://nbdev.fast.ai). The nbdev home page is a more idiomatic example of how to use RenderScripts than the current page's source code -- we're only using RenderScript for the current page to provide a more simple example. In practice, we find that RenderScripts are best used for pages containing a lot of data-driven content, reusable components, and so forth.
 
 You can use RenderScripts to create any kind of file. For instance, the SVG below is created dynamically using [this script](https://github.com/fastai/nbdev/blob/master/nbs/images/circles.svg.py):
+
 """)
 
-a(im('circles.svg', 40))
+print(im('circles.svg', 40))
 
-a("""
+print("""
 Once you've run `nbdev_preview` or `nbdev_docs` you'll find your rendered document in the `_proc` directory, along with all of your processed notebooks. This can be helpful for debugging. You can also simply call your script directly from the shell (e.g. `python renderscript.qmd.py`) to view the printed output.""")
-
-print('\n'.join(r))
 
